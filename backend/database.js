b2bca9207b3283893899fdd72624ed74427a29b0
emilylari <emilylarissapereiradasilaemil@gmail.com>
2026-09-01 18:14:26 -0300

    first commit

---
+++ backend/database.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+// Cole sua string de conexão do Neon aqui ou utilize variável de ambiente
+const pool = new Pool({
+  connectionString: process.env.String_Connection,
+});
+
+async function testarConexao() {
+  try {
+    const client = await pool.connect();
+    const res = await client.query('SELECT NOW()');
+    console.log('Conexão bem-sucedida! Hora atual do banco:', res.rows[0].now);
+    client.release();
+  } catch (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+  } finally {
+    await pool.end();
+  }
+}
+
+testarConexao();
